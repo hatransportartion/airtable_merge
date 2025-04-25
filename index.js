4680d66548944f6d94e57efca6eb9f64e3407017
Sandeep <sandeep@Sandeeps-MacBook-Pro-M1-Max.local>
2025-04-25 07:01:58 -0700

    Added dotenv

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const env = require('dotenv').config();
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
